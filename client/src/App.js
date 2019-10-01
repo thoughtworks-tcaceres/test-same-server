ac4e6f706cc28b0eb210f4819d0dc3c8a4a8213d
tylerccaceres <tyler.caceres@gmail.com>
2019-10-01 15:33:47 -0400

    retry with default

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,54 @@
-import React from "react";
-import axios from "axios";
+import React, { Component } from 'react'
+import './App.css'
 
-const App = () => {
-  const btnClick = async () => {
-    try {
-      console.log("i am here");
-      const data = await axios.get("/data");
-      console.log("data : ", data.data);
-    } catch (error) {
-      console.log("error : ", error);
-    }
-  };
+class App extends Component {
+  state = {
+    cow: '',
+    text: ''
+  }
 
-  return (
-    <div className="App">
-      <p>hello world!!</p>
-      <button onClick={btnClick}>clck on me</button>
-    </div>
-  );
-};
+  componentDidMount() {
+    this.fetchCow()
+  }
 
-export default App;
+  fetchCow = async () => {
+    const response = await fetch(`/api/cow`)
+    const initialCow = await response.json()
+    const cow = initialCow.moo
+    this.setState({ cow })
+  }
+
+  customCow = async evt => {
+    evt.preventDefault()
+    const text = this.state.text
+    const response = await fetch(`/api/cow/${text}`)
+    const custom = await response.json()
+    const cow = custom.moo
+    this.setState({ cow, text: '' })
+  }
+
+  handleChange = evt => {
+    this.setState({ [evt.target.name]: evt.target.value })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h3>Text Cow. Moo</h3>
+        <code>{this.state.cow}</code>
+        <form onSubmit={this.customCow}>
+          <label>Custom Cow Text:</label>
+          <input
+            type="text"
+            name="text"
+            value={this.state.text}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Show me a talking cow!</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default App
