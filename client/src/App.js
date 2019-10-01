a24d21eea2b0a1e198c5f317205d34df7c33daa9
tylerccaceres <tyler.caceres@gmail.com>
2019-10-01 17:11:25 -0400

    try

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,23 @@
-import React, { Component } from "react";
-import "./App.css";
+import React from "react";
+import axios from "axios";
 
-class App extends Component {
-  state = {
-    cow: "",
-    text: ""
+const App = () => {
+  const getInfo = async () => {
+    try {
+      const data = await axios.get("/api/data");
+      console.log("data : ", data.data);
+    } catch (err) {
+      console.log("error : ", err);
+    }
   };
-
-  componentDidMount() {
-    this.fetchCow();
-  }
-
-  fetchCow = async () => {
-    const response = await fetch(`/api/cow`);
-    const initialCow = await response.json();
-    const cow = initialCow.moo;
-    this.setState({ cow });
-  };
-
-  customCow = async (evt) => {
-    evt.preventDefault();
-    const text = this.state.text;
-    const response = await fetch(`/api/cow/${text}`);
-    const custom = await response.json();
-    const cow = custom.moo;
-    this.setState({ cow, text: "" });
-  };
-
-  handleChange = (evt) => {
-    this.setState({ [evt.target.name]: evt.target.value });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <h3>Text Cow. Moo. Tyler Caceres.</h3>
-        <code>{this.state.cow}</code>
-        <form onSubmit={this.customCow}>
-          <label>Custom Cow Text:</label>
-          <input type="text" name="text" value={this.state.text} onChange={this.handleChange} />
-          <button type="submit">Show me a talking cow!</button>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <p>testing 123</p>
+      <p>Tyler Caceres</p>
+      <p>Selin Huang</p>
+      <button onClick={getInfo}>click me for data</button>
+    </div>
+  );
+};
 
 export default App;
--- index.js
+++ index.js
@@ -1,42 +1,28 @@
-const express = require('express')
-const cowsay = require('cowsay')
-const cors = require('cors')
-const path = require('path')
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
 // Create the server
-const app = express()
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, "client/build")));
 
 // Serve our api route /cow that returns a custom talking text cow
-app.get('/api/cow/:say', cors(), async (req, res, next) => {
-  try {
-    const text = req.params.say
-    const moo = cowsay.say({ text })
-    res.json({ moo })
-  } catch (err) {
-    next(err)
-  }
-})
-
-// Serve our base route that returns a Hellow World cow
-app.get('/api/cow/', cors(), async (req, res, next) => {
-  try {
-    const moo = cowsay.say({ text: 'Hello World!' })
-    res.json({ moo })
-  } catch (err) {
-    next(err)
-  }
-})
+app.get("/api/data", (req, res) => {
+  return res.json("selin tyler");
+});
 
 // Anything that doesn't match the above, send back the index.html file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname + "/client/build/index.html"));
+// });
 
 // Choose the port and start the server
-const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-  console.log(`Mixing it up on port ${PORT}`)
-})
+  console.log(`Mixing it up on port ${PORT}`);
+});
